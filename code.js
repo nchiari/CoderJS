6b762e9616508fdc4afbb19677ecd8373b54e1c9
Nico Chiari <nchiari@gmail.com>
2022-10-01 21:30:37 +0200

    Primer commit

---
+++ code.js
@@ -0,0 +1,4 @@
+let numero1 = parseInt(prompt("Ingrese un valor")); // Ej: 2
+let numero2 = parseInt(prompt("Ingrese otro valor diferente")); // Ej: 3
+let resultado = numero1 + numero2;
+alert("El resultado es " + resultado); // Resultado = 5
