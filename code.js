8444450352201b8ccfd7ea12c7a6318494e04a47
Nico Chiari <nchiari@gmail.com>
2022-10-01 21:46:58 +0200

    First commit

--- code.js
+++ code.js
@@ -1,4 +1,10 @@
-let numero1 = parseInt(prompt("Ingrese un valor")); // Ej: 2
-let numero2 = parseInt(prompt("Ingrese otro valor diferente")); // Ej: 3
-let resultado = numero1 + numero2;
-alert("El resultado es " + resultado); // Resultado = 5
+let valor1 = prompt("Escribe tu nombre");
+let valor2 = prompt("Ahora tu apellido");
+let entero1 = parseInt(prompt("Escribe un valor entero"));
+let entero2 = parseInt(prompt("Escribe otro valor entero"));
+let flotante1 = parseFloat(prompt("Escribe un valor decimal"));
+let flotante2 = parseFloat(prompt("Escribe otro valor decimal"));
+
+let resultado = valor1 + "\n" + valor2 + "\n" + (entero1 + entero2) + "\n" + (flotante1 + flotante2);
+console.log("Lamentablemente vengo muy atrasado con las clases por cuestiones personales, y todavía no pude ponerme al día. Voy a ponerme porque realmente me interesa el curso. Perdón!");
+alert("El resultado es\n" + resultado);
